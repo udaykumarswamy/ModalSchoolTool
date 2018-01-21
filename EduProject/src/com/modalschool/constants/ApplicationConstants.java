d318b45564dbbd86f929b1713be7970f5fa46fa1
udaykumarswamy <udaykumar.119@gmail.com>
2018-01-22 02:12:53 +0530

    conflict

--- EduProject/src/com/modalschool/constants/ApplicationConstants.java
+++ EduProject/src/com/modalschool/constants/ApplicationConstants.java
@@ -8,4 +8,5 @@ public class ApplicationConstants {
 
 	public static final String checkUser="from LoginUser m where m.userName=:userName and m.password=:password";
 	public static final String updateLastLogin="update LoginUser m set m.lastLogin=:lastLogin where m.userId=:userId";
+
 }
